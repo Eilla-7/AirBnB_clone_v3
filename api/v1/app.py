a6223c50b4c7a26639d1fad8f89e6a0fc7fa2c33
MemanAwad <meman1awad@gmail.com>
2024-04-02 04:50:45 +0200

    fic pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,18 +8,21 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 
-#register blueprint
+# register blueprint
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exception):
     """ tear down function"""
     storage.close()
 
+
 @app.errorhandler(404)
 def handle_error_404(error):
     """ handle not found error 404"""
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
