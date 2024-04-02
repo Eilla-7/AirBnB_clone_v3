2861fafb1ecc43435a51495067ae910c6eb21090
MemanAwad <meman1awad@gmail.com>
2024-04-02 03:30:40 +0200

    add changes

---
+++ api/v1/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""code to start the api"""
+
+from os import getenv
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ tear down function"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
