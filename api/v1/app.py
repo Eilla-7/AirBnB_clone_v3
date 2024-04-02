bbd12351710a541ae4144a930dc56c815b509082
MemanAwad <meman1awad@gmail.com>
2024-04-02 03:23:31 +0200

    change

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"--"
+"""code to start the api"""
 
+from os import getenv
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 
-
-
-
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
