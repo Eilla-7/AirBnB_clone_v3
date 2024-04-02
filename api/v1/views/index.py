6ea045f5e5b706c6ea4c94f6cb6ee11b434bea33
MemanAwad <meman1awad@gmail.com>
2024-04-02 04:20:28 +0200

    edit status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""index.py"""
 
+from flask import jsonify
 from api.v1.views import app_views
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """ return the status ok"""
+    return jsonify({"status": "OK"})
