5cf88acbe721fac798b63d21cf0fb523a6236e9d
Eilla-7 <eilafadel2238@gmail.com>
2024-04-02 04:13:13 +0300

    Simple edits

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 "--"
 
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 
-
-
-
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """--"""
+    storage.close()
