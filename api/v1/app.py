ffd00ba6b838c0b6ef2b142d8207b5c47721a12c
Eilla-7 <eilafadel2238@gmail.com>
2024-04-02 05:09:02 +0300

    Run Flask Server

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """code to start the api"""
 
 from os import getenv
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -17,4 +18,6 @@ def teardown(exception):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
